5f92c7076ad09703af0b889f1fdb9ff5ea149d20
Alexey V. Ryazhskikh <alexey.r@toptal.com>
2020-11-28 17:04:39 +0200

    use github instead

--- commands.bat
+++ commands.bat
@@ -2,6 +2,6 @@ rem IP address for VPN server
 set VPS_PUBLIC_IP=1.2.3.4
 
 rem download install-vpn.sh from git to server and execute
-ssh ary@%VPS_PUBLIC_IP% "wget -O - https://gist.githubusercontent.com/musukvl/63367edcc9ae643baf1aac4a348a6828/raw/ac2a2194af647f82edc78febd40d205dd4e7c12e/install-vpn.sh | bash"
+ssh ary@%VPS_PUBLIC_IP% "wget -O - https://raw.githubusercontent.com/musukvl/fast-openvpn-setup/main/install-vpn.sh | bash"
 rem download vpn client file to local
 scp ary@%VPS_PUBLIC_IP%:/home/ary/vpn/openvpn_conf/client.ovpn  .
